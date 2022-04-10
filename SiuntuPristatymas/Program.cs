3ab8d71f731d784da138e3e02778b81fe10c7c85
Borouch <60584457+Borouch@users.noreply.github.com>
2022-04-10 13:05:18 +0300

    fix program.cs

--- SiuntuPristatymas/Program.cs
+++ SiuntuPristatymas/Program.cs
@@ -20,10 +20,6 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
-builder.Services
-    .AddScoped<IRepository<Parcel>, ParcelRepository>();
-
-builder.Services.AddControllersWithViews();
 
 builder.Services
     .AddScoped<IRepository<Parcel>, ParcelRepository>()
